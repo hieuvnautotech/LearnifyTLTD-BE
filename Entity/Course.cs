50e1cf7d039f38f1fb80297e55d7a789c5529496
hieu <hieuvnautotech@gmail.com>
2025-11-02 12:08:11 +0700

    ok

---
+++ Entity/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entity
+{
+    public class Course : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string Instructor { get; set; }
+        public decimal Rating { get; set; }
+        public string Image { get; set; } = "https://learnify-assets.s3.amazonaws.com/Images/learnify.png";
+        public string Level { get; set; }
+        public string Language { get; set; }
+        public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
